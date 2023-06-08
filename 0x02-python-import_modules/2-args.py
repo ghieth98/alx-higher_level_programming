81b2c11efbb86a646f8c8609626ba52e4bc81ba1
ghieth98 <hossamghieth@gmail.com>
2023-06-08 07:58:47 -0700

    argv task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+if len(argv) == 1:
+    print("{:d} arguments.".format(len(argv) - 1))
+else:
+    if lent(argv) == 2:
+        print("{:d} arguments.".format(len(argv) - 1))
+    else:
+        print("{:d} arguments.".format(len(argv) - 1))
+
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
