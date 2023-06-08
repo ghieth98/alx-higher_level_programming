3309eeb8580f597c508ca581bb1df4c03e029c6e
ghieth98 <hossamghieth@gmail.com>
2023-06-08 07:47:01 -0700

    fix calculation task

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, div, mul
+    from calculator_1 import add, sub, mul, sub
 
     a = 10
     b = 5
 
     add_result = add(a, b)
     sub_result = sub(a, b)
-    div_result = div(a, b)
     mul_result = mul(a, b)
+    div_result = div(a, b)
+
 
     print("{:d} + {:d} = {:d}".format(a, b, add_result))
     print("{:d} - {:d} = {:d}".format(a, b, sub_result))
-    print("{:d} / {:d} = {:d}".format(a, b, div_result))
     print("{:d} * {:d} = {:d}".format(a, b, mul_result))
+    print("{:d} / {:d} = {:d}".format(a, b, div_result))
