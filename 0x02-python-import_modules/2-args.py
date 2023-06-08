94bb02fbd9becb17c94c65e602f68d49a26f280b
ghieth98 <hossamghieth@gmail.com>
2023-06-08 08:00:05 -0700

    fix args task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
 if len(argv) == 1:
     print("{:d} arguments.".format(len(argv) - 1))
 else:
-    if lent(argv) == 2:
-        print("{:d} arguments.".format(len(argv) - 1))
+    if len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
     else:
-        print("{:d} arguments.".format(len(argv) - 1))
+        print("{:d} arguments:".format(len(argv) - 1))
 
-        for i in range(1, len(argv)):
-            print("{:d}: {:s}".format(i, argv[i]))
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
