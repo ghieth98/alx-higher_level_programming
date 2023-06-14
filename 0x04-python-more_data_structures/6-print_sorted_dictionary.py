a181a140e31b09459bb3581172c32f33279042c1
ghieth98 <hossamghieth@gmail.com>
2023-06-14 09:13:04 -0700

    print sorted dicitonary task

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sort_keys = sorted(list(a_dictionary.keys()))
+    for key in sort_keys:
+        print("{}: {}".format(key, a_dictionary[key]))
