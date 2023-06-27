547927781eafdc8827c6222cf4b1717cc686170e
ghieth98 <hossamghieth@gmail.com>
2023-06-27 09:18:58 -0700

    fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a Square class."""
 
-"""Model that defines square class"""
 
 class Square:
-    """Square class that defines a square"""
+    """Square class that defines a square."""
+
     def __init__(self):
         """Initializes a new private instance of the Square class."""
-    pass
+        pass
