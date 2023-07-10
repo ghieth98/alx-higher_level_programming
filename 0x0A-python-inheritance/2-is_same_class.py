4d74fcbbdd9a0f0bd7a7fb9bc9cf40887a9e59c4
ghieth98 <hossamghieth@gmail.com>
2023-07-10 08:25:06 -0700

    fixed second task comments

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-function that returns True if the object is exactly an instance of the specified class ; otherwise False
+function that returns True if the object is
+exactly an instance of the specified class ;
+otherwise False
 """
 
 
