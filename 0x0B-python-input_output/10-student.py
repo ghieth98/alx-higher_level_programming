e72ffc1995e8987c7562fdf923599cad81913c61
ghieth98 <hossamghieth@gmail.com>
2023-07-11 09:06:01 -0700

    student 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module class Student
+"""
+
+
+class Student:
+    """Defines student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of the student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
