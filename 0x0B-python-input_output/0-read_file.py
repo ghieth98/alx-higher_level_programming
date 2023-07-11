68c67e24659aa7f39447ebbf0e94f1391c7232c5
ghieth98 <hossamghieth@gmail.com>
2023-07-11 06:31:45 -0700

    fixed read fiel task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ function that reads a text file
 
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout"""
-    with open('filename', 'r', encoding="utf-8") as file:
-        print(file.read(), end="")
+    with open('filename', 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
