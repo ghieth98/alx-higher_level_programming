fd8a3a7cb69029de81620b923d6a3b0cc0adaad0
ghieth98 <hossamghieth@gmail.com>
2023-07-11 07:31:37 -0700

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module to writes a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+       function that writes a string to text file (UTF8)
+     Args:
+         filename - name of the file to write to.
+         text(str) - the string to write
+     Return:
+         Number of character written.
+         """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
