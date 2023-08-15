00608561e7a04f7a314d628fef707633e72b909e
ghieth98 <hossamghieth@gmail.com>
2023-08-15 09:56:48 -0700

    top score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- script taht lists all records of the table second table
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---Script taht creats a table second table in the database
+-- Script taht creats a table second table in the database
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Jhon", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
