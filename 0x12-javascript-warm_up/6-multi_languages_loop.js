83989a9183f181e1762ebba0cb60fc60892302d2
ghieth98 <hossamghieth@gmail.com>
2023-09-11 14:21:05 -0700

    task6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+lines.forEach((line) => { console.log(line); });
