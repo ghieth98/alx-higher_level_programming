1b9dbc92f35e2aa723ab5bd4c0a2c77b241e7cfa
ghieth98 <hossamghieth@gmail.com>
2023-09-12 12:35:17 -0700

    fix task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 // Rectangle class that defines a rectangle.
 
 class Rectangle {
-    constructor(w, h) {
+    constructor (w, h) {
         if ((w > 0) && (h > 0)) {
             this.width = w;
             this.height = h;
