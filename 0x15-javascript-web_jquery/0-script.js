5fd0673b29130fefd6ba8ee394f2bd61f5f4fe3c
ghieth98 <hossamghieth@gmail.com>
2023-11-02 07:34:17 -0700

    task 0 to 2

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,2 @@
+// Update the text color of the <header> element to red
+document.querySelector('header').style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,2 @@
+// Use jquery Api to change the text color of the header element to red
+$('HEADER').css('color', '#FF0000');
